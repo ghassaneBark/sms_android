c6e8fc1ac8e38106d3f46b82906d98410115d852
Ghassane Bark <ghassane.bark@gmail.com>
2026-09-03 09:28:59 +0000

    On dev: dev-only version bump in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ma.sms.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.1.31"
+        versionCode = 34
+        versionName = "1.1.32"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.ma.sms.android"
     }
 
