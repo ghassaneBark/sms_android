f1b353d86b81e11b2d1d6f5c5831911bb0ed294d
Ghassane Bark <ghassane.bark@gmail.com>
2026-09-03 10:32:33 +0000

    chore(android): bump version to 1.1.29 (31)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ma.sms.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.1.28"
+        versionCode = 31
+        versionName = "1.1.29"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.ma.sms.android"
     }
 
